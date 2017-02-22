e369aaa6154ec518b0e3c0af46d1105584ea1780
sujishen <sujishen@oneapm.com>
2017-02-22 18:32:18 +0800

    [Add]: Add the ssi-uploaderS

---
+++ src/main/webapp/js/file/file.js
@@ -0,0 +1,33 @@
+$().ready(function() {
+
+	$('#ssi-upload').ssi_uploader({
+		url:'#',
+		maxFileSize:6,
+		allowed:['jpg','gif','txt','png','pdf'],
+		data: {},
+        locale: 'en',
+        preview: true,
+        dropZone: true,
+        maxNumberOfFiles: '',
+        responseValidation: false,
+        ajaxOptions: {},
+        onUpload: function () {
+        },
+        onEachUpload: function () {
+        },
+        beforeUpload: function () {
+        },
+        beforeEachUpload: function () {
+        },
+        errorHandler: {
+            method: function (msg) {
+                alert(msg);
+            },
+            success: 'success',
+            error: 'error'
+        }
+	});
+	//$('#ssi-upload2').ssi_uploader({url:'#',preview:false,allowed:['jpg','gif','txt','png','pdf']});
+	//$('#ssi-upload3').ssi_uploader({url:'#',dropZone:false,allowed:['jpg','gif','txt','png','pdf']});
+	
+});
