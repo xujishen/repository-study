59759626fbf9d8025ee8fd899a4e804a05e21587
sujishen <xujishen_19870312@163.com>
2017-08-19 22:12:57 +0800

    [Update]: 更新一些东西

--- springmvc/src/main/webapp/js/common/common.js
+++ springmvc/src/main/webapp/js/common/common.js
@@ -1,13 +1,13 @@
 var _CONTEXTPATH = '/yourtest';
 
-var _INCLUDES =	  '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/jquery-3.1.1.min.js"></script>'
-				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/json2.js"></script>'
-				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/bootstrap-datetimepicker.js"></script>'
-				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/layer/layer.js"></script>'
-				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/dateFormat.js"></script>'
-			   	+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/common-function.js"></script>'
-				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/Timer.js"></script>'
-				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/CommonBean.js"></script>';
+var _INCLUDES =	  '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/jquery-3.1.1.min.js?num=' + Math.random() + '"></script>'
+				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/json2.js?num=' + Math.random() + '"></script>'
+				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/bootstrap-datetimepicker.js?num=' + Math.random() + '"></script>'
+				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/layer/layer.js?num=' + Math.random() + '"></script>'
+				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/dateFormat.js?num=' + Math.random() + '"></script>'
+			   	+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/common-function.js?num=' + Math.random() + '"></script>'
+				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/Timer.js?num=' + Math.random() + '"></script>'
+				+ '<script type="text/javascript" src="' + _CONTEXTPATH + '/js/common/CommonBean.js?num=' + Math.random() + '"></script>';
 document.write(_INCLUDES);
 
 var EMPTY = '';
