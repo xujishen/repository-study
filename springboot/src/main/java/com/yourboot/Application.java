8d851943d0eba5f610a0dfef60f6225e851e638f
sujishen <sujishen@oneapm.com>
2018-08-30 18:14:27 +0800

    update

--- springboot/src/main/java/com/yourboot/Application.java
+++ springboot/src/main/java/com/yourboot/Application.java
@@ -14,9 +14,9 @@ import org.springframework.test.context.ContextConfiguration;
  */
 @SpringBootApplication
 //@ContextConfiguration(locations = {"classpath:application.properties"})
-@EnableAspectJAutoProxy(exposeProxy=true)
+@EnableAspectJAutoProxy(exposeProxy=true, proxyTargetClass = false)
 @EnableAutoConfiguration
-//@ComponentScan(basePackages={"com.yourboot"})
+@ComponentScan(basePackages={"com.yourboot"})
 @ImportResource(locations = {"classpath:spring-mybatis.xml"})
 public class Application extends SpringBootServletInitializer {
 
