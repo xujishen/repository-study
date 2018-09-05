6220b2b9aa16459e1b804109b936c9b41ea6b6ad
sujishen <xujishen_19870312@163.com>
2018-09-05 23:45:28 +0800

    add boot

--- springboot/src/main/java/com/yourboot/Application.java
+++ springboot/src/main/java/com/yourboot/Application.java
@@ -7,15 +7,16 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * Created By shen on 2018-5-18 22:50
  */
 @SpringBootApplication
-//@ContextConfiguration(locations = {"classpath:application.properties"})
+@ContextConfiguration(locations = {"classpath:application.properties"})
 @EnableAspectJAutoProxy(exposeProxy=true, proxyTargetClass = false)
-@EnableAutoConfiguration
-@ComponentScan(basePackages={"com.yourboot"})
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages={"com.yourboot"})
 @ImportResource(locations = {"classpath:spring-core.xml"})
 public class Application extends SpringBootServletInitializer {
 
